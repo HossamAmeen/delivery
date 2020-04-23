a3dade8ec7908974f6b17e82108cff84fe8e537c
hosam ameen <hosamameen948@gmail.com>
2020-04-23 16:26:45 +0200

    add notification to client

--- app/Http/Controllers/Dashboard/OrderController.php
+++ app/Http/Controllers/Dashboard/OrderController.php
@@ -204,7 +204,7 @@ class OrderController extends BackEndController
 
          $snapshot = $reference->getSnapshot()->getValue();
 
-         $snapshot[$clinetId] =$orderID;
+         $snapshot[$clinetId] ="$orderstatus-$orderID";
          $newPost = $database
              ->getReference('/clients')
              ->update($snapshot);
